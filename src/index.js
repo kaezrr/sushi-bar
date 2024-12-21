519e68c31aa6fbdfc553d688cbd0ca4803e2974e
kaezrr <107052359+kaezrr@users.noreply.github.com>
2024-12-21 20:10:09 +0530

    Finish menu page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import './style.css';
 import { loadHome } from './home.js';
+import { loadMenu } from './menu.js';
 
+// Load home page when website is first loaded
 loadHome();
+
+const content = document.querySelector('#content');
+const nav = document.querySelector('nav');
+nav.addEventListener('click', (e) => {
+    content.replaceChildren();
+    switch (e.target.textContent) {
+        case 'HOME':
+            loadHome();
+            return;
+        case 'MENU':
+            loadMenu();
+            return;
+        case 'CONTACT':
+            return;
+    }
+});
---
+++ src/menu.js
@@ -0,0 +1,33 @@
+import sushiPath1 from '../images/makizushi.jpg';
+import sushiPath2 from '../images/inarizushi.jpg';
+import sushiPath3 from '../images/nigirizushi.jpg';
+import sushiPath4 from '../images/oshizushi.jpg';
+
+function createMenuCard(imgSrc, name, price) {
+    const card = document.createElement('div');
+    card.classList = 'card';
+    const h3 = document.createElement('h3');
+    h3.textContent = `${name} - \$${price.toFixed(2)}`;
+    const img = document.createElement('img');
+    img.src = imgSrc;
+    card.appendChild(img);
+    card.appendChild(h3);
+    return card;
+}
+
+function loadMenu() {
+    const menu = document.createElement('div');
+    menu.classList = 'menu';
+    menu.appendChild(createMenuCard(sushiPath1, 'Makizushi', 5.0));
+    menu.appendChild(createMenuCard(sushiPath2, 'Inarizushi', 3.5));
+    menu.appendChild(createMenuCard(sushiPath3, 'Nigirizushi', 7.5));
+    menu.appendChild(createMenuCard(sushiPath4, 'Oshizushi', 4.0));
+    const mainContent = document.querySelector('#content');
+    const h1 = document.createElement('h1');
+    h1.classList = 'menu';
+    h1.textContent = 'Taste the menu';
+    mainContent.appendChild(h1);
+    mainContent.appendChild(menu);
+}
+
+export { loadMenu };
